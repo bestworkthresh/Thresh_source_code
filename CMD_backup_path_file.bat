2ac25cb0e71ee35424248f31fd1bd5be9cdf1593
Thresh1127 <79625788+bestworkthresh@users.noreply.github.com>
2021-10-25 11:44:59 +0800

    20211025_FRS_網頁上傳

--- CMD_backup_path_file.bat
+++ CMD_backup_path_file.bat
@@ -4,5 +4,5 @@
 ::此語法感謝懿萱技術支援
 set Date_folder=%date:~0,4%_%date:~5,2%_%date:~8,2%"("%date:~11,2%")_"
 
-xcopy "S:\資訊部\999.資訊部人員專屬空間\洪金霖" "D:\01A_Thresh\SSS.共用槽備份\" /d/k/e/y/c/I/h > %Date_folder%errorlog.txt
-xcopy "S:\資訊部\35.資料庫相關" "D:\01A_Thresh\SSS.資料庫共用槽備份\" /d/k/e/y/c/I/h >> %Date_folder%errorlog.txt
+xcopy "S:\資訊部\999.資訊部人員專屬空間\洪金霖" "D:\01A_Thresh\SSS.backup_data_everyday\SSS.共用槽備份\" /d/k/e/y/c/I/h >D:\01A_Thresh\SSS.backup_data_everyday\log\%Date_folder%errorlog.txt
+xcopy "S:\資訊部\35.資料庫相關" "D:\01A_Thresh\SSS.backup_data_everyday\SSS.資料庫共用槽備份\" /d/k/e/y/c/I/h >> D:\01A_Thresh\SSS.backup_data_everyday\log\%Date_folder%errorlog.txt
